b14a21aaed53edd72aa2636858c652463a91b139
Mahek Mehra <148412722+mahekmehra@users.noreply.github.com>
2026-06-23 11:22:57 +0530

    Time: 4 ms (27.52%), Space: 9.3 MB (45.7%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    vector<int> dp;
+
+    int help(vector<int>& nums, int target){
+
+        if(target==0) return 1;
+        if(target<0) return 0;
+
+        if(dp[target]!=-1) return dp[target];
+
+        int ways = 0;
+
+        for(int i=0;i<nums.size();i++){
+            ways += help(nums,target-nums[i]);
+
+        }
+
+        return dp[target] = ways;
+    }
+
+
+    int combinationSum4(vector<int>& nums, int target) {
+
+        dp.resize(target+1,-1);
+        return help(nums,target);
+        
+    }
+};
