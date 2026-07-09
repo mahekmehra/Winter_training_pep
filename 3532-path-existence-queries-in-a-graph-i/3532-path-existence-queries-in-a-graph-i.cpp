09675f154943c6b0e125b05e921f07931950e62c
Mahek Mehra <148412722+mahekmehra@users.noreply.github.com>
2026-07-09 18:05:50 +0530

    Time: 13 ms (66.3%), Space: 224.1 MB (78.36%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,49 +1,22 @@
 class Solution {
 public:
 
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& comp, int id) {
-        comp[node] = id;
-
-        for (int nxt : adj[node]) {
-            if (comp[nxt] == -1) {
-                dfs(nxt, adj, comp, id);
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
+                                      vector<vector<int>>& queries) {
+        vector<int> group(n, 0);
+        int currentGroup = 0;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                currentGroup++;
             }
-        }
-    }
-
-    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-
-        vector<vector<int>> adj(n);
-
-        for (int i = 0; i < n; i++) {
-            adj[i].push_back(i);
+            group[i] = currentGroup;
         }
 
-        // Build graph
-        for (int i = 0; i < n - 1; i++) {
-            if (abs(nums[i + 1] - nums[i]) <= maxDiff) {
-                adj[i].push_back(i + 1);
-                adj[i + 1].push_back(i);
-            }
-        }
-
-        // Find connected components
-        vector<int> comp(n, -1);
-        int id = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (comp[i] == -1) {
-                dfs(i, adj, comp, id);
-                id++;
-            }
-        }
-
-        // Answer queries
         vector<bool> res;
-        for (auto &q : queries) {
-            res.push_back(comp[q[0]] == comp[q[1]]);
+        for (auto& q : queries) {
+            int u = q[0], v = q[1];
+            res.push_back(group[u] == group[v]);
         }
-
         return res;
     }
 };
