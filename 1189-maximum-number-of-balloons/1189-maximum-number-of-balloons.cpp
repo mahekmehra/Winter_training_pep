ed1b8f0abde9e51f74b1e11bb3ffa6af9b371a93
Mahek Mehra <148412722+mahekmehra@users.noreply.github.com>
2026-07-13 12:05:32 +0530

    Time: 0 ms (100%), Space: 9.2 MB (6.77%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+
+        unordered_map<char,int> cnt;
+
+        for(char c : text)
+            cnt[c]++;
+
+        return min({
+            cnt['b'],
+            cnt['a'],
+            cnt['l']/2,
+            cnt['o']/2,
+            cnt['n']
+        });
+     
+    }
+};
