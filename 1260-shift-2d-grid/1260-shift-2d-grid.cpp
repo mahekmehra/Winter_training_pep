b09c463faac93748926a4fc7b46509fbd0feb445
Mahek Mehra <148412722+mahekmehra@users.noreply.github.com>
2026-07-20 22:31:49 +0530

    Time: 3 ms (45.51%), Space: 18.9 MB (20.65%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
 
-        vector<int> res;
+        /*vector<int> res;
         int n = grid.size();
         int m = grid[0].size();
         for(int i=0;i<n;i++){
@@ -22,6 +22,32 @@ public:
                 grid[i][j]  = res[i*m+j];
             }
         }
+
+        return grid;*/
+
+        int n = grid.size();
+        int m = grid[0].size();
+        int size = n * m;
+
+        k %= size;
+
+        vector<int> flat(size);
+
+        // Flatten
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                flat[i * m + j] = grid[i][j];
+
+        vector<int> rotated(size);
+
+        // Shift right
+        for (int i = 0; i < size; i++)
+            rotated[(i + k) % size] = flat[i];
+
+        // Convert back
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                grid[i][j] = rotated[i * m + j];
 
         return grid;
         
