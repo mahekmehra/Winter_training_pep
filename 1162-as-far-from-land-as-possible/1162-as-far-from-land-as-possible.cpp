ae218695f7886cee451a092a59cfa5a7ebfcd2ee
Mahek Mehra <148412722+mahekmehra@users.noreply.github.com>
2026-08-11 13:29:49 +0530

    Time: 15 ms (68.74%), Space: 23.8 MB (82.47%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,48 +1,54 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-
         int n = grid.size();
         int m = grid[0].size();
 
-        vector<vector<int>>ans(n,vector<int>(m,-1));
+        queue<pair<int, int>> q;
 
-        int ansmax = INT_MIN;
-        queue<pair<int,int>> q;
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    q.push({i,j});
-                    ans[i][j] = 0;
+        // Land = -1 means source / distance 0
+        // Water = 0 means unvisited
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    q.push({i, j});
+                    grid[i][j] = -1;
                 }
             }
         }
 
-        vector<int>dx = {-1,1,0,0};
-        vector<int>dy = {0,0,1,-1};
+        vector<int> dx = {-1, 1, 0, 0};
+        vector<int> dy = {0, 0, 1, -1};
 
-        while(!q.empty()){
+        int maxDistance = -1;
 
-            int row = q.front().first;
-            int col = q.front().second;
+        while (!q.empty()) {
+            auto [row, col] = q.front();
             q.pop();
 
-            for(int i=0;i<4;i++){
-                int cr = row + dx[i];
-                int cc = col + dy[i];
+            for (int i = 0; i < 4; i++) {
+                int nr = row + dx[i];
+                int nc = col + dy[i];
 
-                if(cr>=0 && cc>=0 &&  cr<n && cc<m && ans[cr][cc]==-1){
-                    q.push({cr,cc});
-                    ans[cr][cc] = ans[row][col]+1;
-                    ansmax = max(ansmax,ans[cr][cc]);
+                if (nr >= 0 && nr < n &&
+                    nc >= 0 && nc < m &&
+                    grid[nr][nc] == 0) {
+
+                    // Land(-1) -> distance 0
+                    // Water distance = parent distance + 1
+                    int currentDistance = (grid[row][col] == -1)
+                                            ? 0
+                                            : grid[row][col];
+
+                    grid[nr][nc] = currentDistance + 1;
+
+                    maxDistance = max(maxDistance, grid[nr][nc]);
+
+                    q.push({nr, nc});
                 }
             }
-
-            
         }
-        return (ansmax==INT_MIN)? -1 : ansmax;
 
-        
+        return maxDistance;
     }
 };
