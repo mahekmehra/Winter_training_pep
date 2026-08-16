8267c5a136b9164fb598e06af4ca4f3265373cd8
Mahek Mehra <148412722+mahekmehra@users.noreply.github.com>
2026-08-16 19:04:51 +0530

    Time: 19 ms (28.55%), Space: 38.6 MB (33.25%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -39,7 +39,31 @@ public:
         return count; */
 
         //sliding window approach
-        return func(nums,goal) - func(nums,goal-1);
+        //return func(nums,goal) - func(nums,goal-1);
+
+        //hashmap solution
+        unordered_map<int, int> prefixSumCount;
+
+        // Initialize count of valid subarrays and current sum
+        int count = 0, sum = 0;
+
+        // Add base case: prefix sum 0 has frequency 1
+        prefixSumCount[0] = 1;
+
+        // Iterate through the array
+        for (int num : nums) {
+            // Add current element to prefix sum
+            sum += num;
+
+            // If (sum - goal) exists in map, add its frequency to count
+            if (prefixSumCount.find(sum - goal) != prefixSumCount.end()) {
+                count += prefixSumCount[sum - goal];
+            }
+
+            // Increment frequency of current prefix sum
+            prefixSumCount[sum]++;
+        }
+        return count;
         
     }
 };
