9055f6c4818c2474241c97b2a40d5298292f27be
Mahek Mehra <148412722+mahekmehra@users.noreply.github.com>
2026-08-19 00:33:43 +0530

    Time: 87 ms (15.92%), Space: 113.6 MB (9.13%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,14 +2,27 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
 
-        unordered_set<int> st;
-        for(int i=0;i<nums.size();i++){
-            if(st.find(nums[i])!=st.end()){
-                return true;
+        //brute force
+        /*int n = nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[j]==nums[i]){
+                    return true;
+                }
             }
+        }
+        return false;*/
+        
+        //optimal approach  
+        int n =nums.size();
+        unordered_set<int>st;
+        for(int i=0;i<n;i++){
             st.insert(nums[i]);
         }
-        return false;
+
+        return (n!=st.size())? true : false;
+
+
         
     }
 };
