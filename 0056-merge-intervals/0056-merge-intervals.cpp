cf6446d8c598f66c6df27802cd36e016ea40e7fd
Mahek Mehra <148412722+mahekmehra@users.noreply.github.com>
2026-08-21 00:23:26 +0530

    Time: 3 ms (89.03%), Space: 23.8 MB (59.61%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -9,7 +9,7 @@ public:
 
         vector<vector<int>> merged;
         merged.push_back(intervals[0]);
-        for(int i=0;i<intervals.size();i++){
+        for(int i=1;i<intervals.size();i++){
             if(merged.back()[1]>=intervals[i][0]){
                 merged.back()[1] = max(merged.back()[1],intervals[i][1]);
             }else{
