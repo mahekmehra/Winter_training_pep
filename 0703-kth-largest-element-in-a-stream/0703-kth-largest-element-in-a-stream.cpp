90fdacd02b9b27df607ec1000a9c3a18b3bdab88
Mahek Mehra <148412722+mahekmehra@users.noreply.github.com>
2026-08-28 23:41:50 +0530

    Time: 3 ms (90.96%), Space: 33.1 MB (62.09%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -8,29 +8,16 @@ public:
 
     KthLargest(int k, vector<int>& nums) :k(k) {
 
-        for(int num:nums){
-            if(pq.size()<k){
-                pq.push(num);
-            }else{
-                if(num>pq.top()){
-                    pq.pop();
-                    pq.push(num);
-                }
-            }
+        for(int num : nums){
+            pq.push(num);
+            if(pq.size()>k) pq.pop();
         }
     }
     
     int add(int val) {
 
-        if(pq.size()<k){
-            pq.push(val);
-        }else{
-            if(val>pq.top()){
-                pq.pop();
-                pq.push(val);
-            
-            }
-        }
+        pq.push(val);
+        if(pq.size()>k) pq.pop();
         return pq.top();
     }
 };
